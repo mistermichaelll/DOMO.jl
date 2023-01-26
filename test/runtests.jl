f5330fea421b163eec78a7ee2cd71384d7fa77d0
Michael Johnson <mistermichaellll@gmail.com>
2023-01-26 17:12:27 -0500

    add test for type conversion

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,15 @@ using DataFrames
 include("test-sets/schema-tests.jl")
 
 @testset "JuDOMO.jl" begin
-    ## test whether create_dataset_schema() creates the correct structure on a simple dataset.
+    # test whether create_dataset_schema() creates the correct structure on a simple dataset.
     @test create_dataset_schema(
         schema_test_mathematicians_dataset,
         "Leonhard Euler Party",
         "Mathematician guest list."
     ) == schema_test_mathematicians
+    # test whether types match
+    #  (note that mapping here can give a more helpful error)
+    map(1:length(types)) do a
+        @test match_domo_types(types[a]) == expected_types[a]
+    end
 end
--- test/test-sets/schema-tests.jl
+++ test/test-sets/schema-tests.jl
@@ -1,6 +1,11 @@
 import JuDOMO: match_domo_types, create_dataset_schema
 import JSON: json
 
+## test sets for matching Julia's types to Domo's
+types = [Int32, Int64, Float32, Float64, String]
+expected_types = ["DOUBLE", "DOUBLE", "DECIMAL", "DECIMAL", "STRING"]
+
+## test sets for schema creation
 schema_test_mathematicians = Dict(
     "name" => "Leonhard Euler Party",
     "description" => "Mathematician guest list.",
