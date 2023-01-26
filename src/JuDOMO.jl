690a7901796aee7ead363d8ccebb32e17ce529aa
Michael Johnson <mistermichaellll@gmail.com>
2023-01-26 01:20:36 -0500

    make utils available across packages

--- src/JuDOMO.jl
+++ src/JuDOMO.jl
@@ -1,8 +1,9 @@
 module JuDOMO
 include("authentication.jl")
 include("datasets.jl")
+include("utils.jl")
 
-import HTTP: Response, request, iserror
+import HTTP: request, iserror
 import JSON: parse, json
 import Base64: base64encode
 import DataFrames: DataFrame, nrow, ncol
