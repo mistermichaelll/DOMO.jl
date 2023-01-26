00e401a9949f22fb6ddfa4c68fe0735da0095249
Michael Johnson <mistermichaellll@gmail.com>
2023-01-26 00:16:19 -0500

    add function to send PUT to datasets

--- src/utils.jl
+++ src/utils.jl
@@ -4,3 +4,13 @@ function parse_HTTP_response(http_response::Response)
         String(http_response.body)
     )
 end
+
+function PUT_data(data::String, dataset_id::String, access_token::String)
+    response = request(
+        "PUT",
+        "https://api.domo.com/v1/datasets/" * dataset_id * "/data",
+        ["Content-Type" => "text/csv", "Authorization" => "bearer " * access_token],
+        data;
+        status_exception = false
+    )
+end
