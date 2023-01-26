e727c89d77017d2e4304ca246c34181b537439a7
Michael Johnson <mistermichaellll@gmail.com>
2023-01-26 17:37:52 -0500

    add dep

--- test/test-sets/schema-tests.jl
+++ test/test-sets/schema-tests.jl
@@ -1,5 +1,6 @@
 import JuDOMO: match_domo_types, create_dataset_schema
 import JSON: json
+using Dates
 
 ## test sets for matching Julia's types to Domo's
 types = [Int32, Int64, Float32, Float64, String, Dates.Date, Dates.DateTime]
