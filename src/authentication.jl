fb74ee61639e79c58df7f7ae7c8830204a60e1d7
Michael Johnson <mistermichaellll@gmail.com>
2023-01-25 21:19:22 -0500

    add some error handling

--- src/authentication.jl
+++ src/authentication.jl
@@ -1,4 +1,4 @@
-import HTTP: response, request
+import HTTP: Response, request, iserror
 import JSON: parse
 import Base64: base64encode
 include("utils.jl")
@@ -8,17 +8,19 @@ function create_basic_auth_header(client_id::String, client_secret::String)
 end
 
 function DOMO_auth(client_id::String, client_secret::String)
+     # this likely would never happen since providing nothing will throw a
+     #  method error...and I doubt someone would pass an empty string...but you never know.
     if isempty(client_id) | isempty(client_secret)
-        error("Don't you need some auth codes or something...?")
+        error("Missing client keys - if you don't have them, create them here: https://developer.domo.com/new-client")
     end
 
     url = "https://api.domo.com/oauth/token?grant_type=client_credentials&scope=data"
     authorization = create_basic_auth_header(client_id, client_secret)
 
-    response = request("GET", url, authorization)
+    response = request("GET", url, authorization; status_exception = false)
 
-    if response.status != 200
-        error("Response had error: " * response.status)
+    if iserror(response)
+        error("Response had error: " * string(response.status))
     end
 
     global domo = parse_HTTP_response(response)
