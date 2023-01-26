fe8b0a70dfbb124d1fa89c0b2aa1adf5f5415863
Michael Johnson <mistermichaellll@gmail.com>
2023-01-26 17:37:34 -0500

    clean up run tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,9 @@
 using JuDOMO
 using Test
-using DataFrames
 include("test-sets/schema-tests.jl")
 
 @testset "JuDOMO.jl" begin
-    # test whether create_dataset_schema() creates the correct structure on a simple dataset.
+    # test whether create_dataset_schema() creates the correct structure on a simple dataset
     @test create_dataset_schema(
         schema_test_mathematicians_dataset,
         "Leonhard Euler Party",
