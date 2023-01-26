ed49ed79afc9d35fa28ef9ae4ab4b313a2a8a325
Michael Johnson <mistermichaellll@gmail.com>
2023-01-26 16:56:45 -0500

    create a cleaner structure for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,30 +1,13 @@
 using JuDOMO
 using Test
 using DataFrames
-import JuDOMO: match_domo_types, create_dataset_schema
-import JSON: json
-
-test_schema = Dict(
-    "name" => "Leonhard Euler Party",
-    "description" => "Mathematician guest list.",
-    "rows" => 3,
-    "schema" => Dict(
-        "columns" => [ Dict(
-            "type" => "STRING",
-            "name" => "Friend"
-        ), Dict(
-            "type" => "STRING",
-            "name" => "Attending"
-        ) ]
-    )
-) |> json
-
-test_dataset = DataFrame(
-    "Friend" => ["Pythagoras", "Alan Turing", "George Boole"],
-    "Attending" => ["TRUE", "TRUE", "FALSE"]
-)
+include("test-sets/schema-tests.jl")
 
 @testset "JuDOMO.jl" begin
     ## test whether create_dataset_schema() creates the correct structure on a simple dataset.
-    @test create_dataset_schema(test_dataset, "Leonhard Euler Party", "Mathematician guest list.") == test_schema
+    @test create_dataset_schema(
+        schema_test_mathematicians_dataset,
+        "Leonhard Euler Party",
+        "Mathematician guest list."
+    ) == schema_test_mathematicians
 end
