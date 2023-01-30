7ea81a8b38bb4cadf86739a901e37f8075532a1f
Michael Johnson <mistermichaellll@gmail.com>
2023-01-30 13:15:00 -0500

    eliminate csv dep

--- src/DOMO.jl
+++ src/DOMO.jl
@@ -6,8 +6,7 @@ include("utils.jl")
 import HTTP: request, iserror
 import JSON: parse, json
 import Base64: base64encode
-import DataFrames: nrow, ncol, rownumber, eachrow
-import CSV: write
+import DataFrames: nrow, ncol, rownumber, eachrow, eachcol
 using Dates
 
 export DOMO_auth
