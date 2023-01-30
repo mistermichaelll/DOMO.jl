d80c50237c9cde0ef0da1ab8a7fb2e1d8500b6eb
Michael Johnson <mistermichaellll@gmail.com>
2023-01-30 13:14:52 -0500

    replace io stuff with new function

--- src/datasets.jl
+++ src/datasets.jl
@@ -32,12 +32,7 @@ function replace_dataset(dataset_id::String, df)
 
     access_token = domo["access_token"]
 
-    io = IOBuffer()
-    write(io, df; newline = "\n", writeheader = false, append = false)
-    data = replace(
-        String(io.data),
-        "\n\0\0\0\0\0\0" => ""
-    )
+    data = create_csv_structure(df)
 
     response = PUT_data(data, dataset_id, access_token)
 
