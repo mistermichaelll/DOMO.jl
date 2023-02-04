684ea7e7e727d2771070ae7b789dcb39a8308687
Michael Johnson <mistermichaellll@gmail.com>
2023-02-03 22:34:05 -0500

    more basic docs

--- src/schema.jl
+++ src/schema.jl
@@ -1,5 +1,10 @@
-# function to match Julia's types to Domo's
-#  according to Docs, accepted values are STRING, DECIMAL, LONG, DOUBLE, DATE, and DATETIME.
+"""
+    match_domo_types(type)
+
+This function helps match types in Julia to those accepted by the Domo API.
+
+This is an internal function used by the package, it may come in handy if you're trying to extend the API.
+"""
 function match_domo_types(type)
     if type == String || type == Union{String, Missing} || type == Bool || type == Union{Bool, Missing}
         "STRING"
@@ -14,8 +19,11 @@ function match_domo_types(type)
     end
 end
 
-# function to set up the dataset schema from a dataframe in the proper format for pushing
-#  the result to Domo.
+"""
+    create_dataset_schema(df, name, description)
+
+Creates a dataset schema for a given DataFrame. This is an internal function, but may be useful if you are extending the Domo API in Julia.
+"""
 function create_dataset_schema(df, name, description)
     column_schema = Dict(
         "columns" => map(1:ncol(df)) do column
@@ -36,7 +44,6 @@ function create_dataset_schema(df, name, description)
     return json(schema)
 end
 
-# create a string for the line of the csv.
 function create_csv_string(row, col_num)
     csv_string = string(ifelse(ismissing(row[col_num]), "", row[col_num]))
 
@@ -47,7 +54,11 @@ function create_csv_string(row, col_num)
     return csv_string
 end
 
-## create csv structure for dataset to be sent to Domo.
+"""
+    create_csv_structure(df)
+
+This function creates a CSV, in the form of a single string, from a DataFrame to send to Domo.
+"""
 function create_csv_structure(df)
     csv_data = ""
 
@@ -66,7 +77,6 @@ function create_csv_structure(df)
     return csv_data
 end
 
-## send the schema to Domo.
 function push_schema_to_domo(dataset_schema)
     response = request(
         "POST",
